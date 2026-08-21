20e4b3dc796d7500ec9efaf8b5e8a8849e38a3c4
santiagobarua-glitch <santiago.barua@gm2dev.com>
2026-08-21 12:41:46 -0300

    feat(clase1/barua): add ClassOne component and styles

---
+++ react-e1/src/clase1/barua/Saludar.tsx
@@ -0,0 +1,11 @@
+function Saludar() {
+    const numeroHinchas = 22000;
+    return(
+        <div>
+            "YO SOY DE ARGENTINOS, NACIDO EN LA PATERNAL Y CON LA CANCHA NUEVA OTRA VUELTA QUIERO DAR"
+            <img src='/src/assets/favicon.jpeg'></img>
+            <p>Argentinos juniors tiene: {numeroHinchas} hinchas! El equipo de barrio más grande del mundo!!!</p>
+        </div>
+    )
+}
+export default Saludar
