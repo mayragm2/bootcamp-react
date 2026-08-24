28a4fa47ecf2ed6df7aa998271ffa7e720ad8325
tobiasmanquez <tobias.manquez@gm2dev.com>
2026-08-24 10:30:06 -0300

    Add new components and styles for user profile display
    - Created Class1.css for styling components.
    - Added ContentOne component to serve as a container for Saludar and TarjetaPerfil components.
    - Implemented Saludar component to greet the user with a message and display a logo.
    - Developed TarjetaPerfil component to showcase user information and an image.
    - Updated tsconfig.app.json to remove unnecessary type definitions.

---
+++ react-e1/src/clase1/Tobias/ContentOne.tsx
@@ -0,0 +1,16 @@
+import Saludar from "./Saludar"
+import TarjetaPerfil from "./TarjetaPerfil"
+import "./Class1.css"
+
+function ContentOne() {
+    return (
+        <div>
+            <Saludar />
+            <TarjetaPerfil />
+        </div>
+
+    )
+
+}
+
+export default ContentOne
