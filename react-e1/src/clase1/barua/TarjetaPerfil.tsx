0dca3b39e4031594f02fa7ed5012fbf21c60baa8
Santiago Barua <santiago.barua@gm2dev.com>
2026-08-24 10:45:44 -0300

    feat(class2/barua): update package-lock and package.json, modify App component import, add image asset, and create Saludo component

--- react-e1/src/clase1/barua/TarjetaPerfil.tsx
+++ react-e1/src/clase1/barua/TarjetaPerfil.tsx
@@ -3,7 +3,7 @@ function TarjetaPerfil() {
         <div>
             <h1>El equipo de barrio más grande:</h1>
             <p>La Asociación Atlética Argentinos Juniors es reconocida con orgullo como "el club de barrio más grande del mundo" por su arraigo profundo en La Paternal y su rica historia futbolística. Jugadores como Maradona, Bichi Borgi, Herni, entre otros salieron de ahí. Su historia no termina solo en eso, teniendo 3 ligas, 1 interamericana y la más importante del continente americano: La Libertadores.</p>
-            <img src="./AAAJJJJJJJJJ.png"></img>
+            <img src="/aaaj.png"></img>
         </div>
     ) 
 }
